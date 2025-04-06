5cba1d2ca310ae9f7dead32936c5d6d6b1dfc698
be2455 <asd12352345@gmail.com>
2025-04-06 23:55:54 +0800

    1. Add new features to technical_indicators.py and volume_features.py 2. Add Bollinger Bands to EDA_Kline.ipynb 3. Add new notebook: EDA_SMA_Slope_Feature.ipynb

--- features/technical_indicators.py
+++ features/technical_indicators.py
@@ -12,13 +12,21 @@ def add_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
     Low   = df['Low']
 
     # ----- RSI -----
-    df['RSI_14'] = ta.momentum.RSIIndicator(close = Close, window = 14).rsi()
+    RSI_14 = ta.momentum.RSIIndicator(close = Close, window = 14).rsi()
+    df['RSI_14']      = RSI_14
+    df['RSI_14_diff'] = RSI_14 - RSI_14.shift(1)
 
     # ----- MACD -----
-    MACD = ta.trend.MACD(close = Close)
-    df['MACD']        = MACD.macd()
-    df['MACD_signal'] = MACD.macd_signal()
-    df['MACD_diff']   = MACD.macd_diff()
+    MACD      = ta.trend.MACD(close = Close)
+    MACD_Diff = MACD.macd_diff()
+    df['MACD']            = MACD.macd()
+    df['MACD_signal']     = MACD.macd_signal()
+    df['MACD_diff']       = MACD_Diff
+    df['MACD_diff_delta'] = MACD_Diff.diff()
+
+    df['MACD_cross_zero'] = 0
+    df.loc[(df['MACD'].shift(1) < 0) & (df['MACD'] >= 0), 'MACD_cross_zero'] = 1   # travel upwards
+    df.loc[(df['MACD'].shift(1) > 0) & (df['MACD'] <= 0), 'MACD_cross_zero'] = -1  # Cross down
 
     # ----- KD (Stochastic Oscillator) -----
     stoch = ta.momentum.StochasticOscillator(high = High, low = Low, close = Close)
@@ -26,10 +34,34 @@ def add_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
     df['D'] = stoch.stoch_signal()
 
     # ----- SMA simple moving average -----
-    df['SMA_5']  = ta.trend.SMAIndicator(close = Close, window =  5).sma_indicator()
-    df['SMA_10'] = ta.trend.SMAIndicator(close = Close, window = 10).sma_indicator()
-    df['SMA_20'] = ta.trend.SMAIndicator(close = Close, window = 20).sma_indicator()
-    df['SMA_60'] = ta.trend.SMAIndicator(close = Close, window = 60).sma_indicator()
+    SMA_5  = ta.trend.SMAIndicator(close = Close, window =  5).sma_indicator()
+    SMA_10 = ta.trend.SMAIndicator(close = Close, window = 10).sma_indicator()
+    SMA_20 = ta.trend.SMAIndicator(close = Close, window = 20).sma_indicator()
+    SMA_60 = ta.trend.SMAIndicator(close = Close, window = 60).sma_indicator()
+
+    df['SMA_5']  = SMA_5
+    df['SMA_10'] = SMA_10
+    df['SMA_20'] = SMA_20
+    df['SMA_60'] = SMA_60
+
+    df['SMA_ratio_5']  = Close / SMA_5
+    df['SMA_ratio_10'] = Close / SMA_10
+    df['SMA_ratio_20'] = Close / SMA_20
+    df['SMA_ratio_60'] = Close / SMA_60
+
+    SMA_slope_5  = SMA_5 - SMA_5.shift(1)
+    SMA_slope_10 = SMA_10 - SMA_10.shift(1)
+    SMA_slope_20 = SMA_20 - SMA_20.shift(1)
+    SMA_slope_60 = SMA_60 - SMA_60.shift(1)
+
+    df['SMA_slope_5']  = SMA_slope_5
+    df['SMA_slope_10'] = SMA_slope_10
+    df['SMA_slope_20'] = SMA_slope_20
+    df['SMA_slope_60'] = SMA_slope_60
+
+    # TODO: Determine the average of N days
+    df['SMA_slope_20_avg_3'] = SMA_slope_20.rolling(3).mean()
+    df['sma_slope_20_avg_5'] = SMA_slope_20.rolling(5).mean()
 
     # ----- EMA exponential moving average -----
     df['EMA_5']  = ta.trend.EMAIndicator(close = Close, window =  5).ema_indicator()
--- features/volume_features.py
+++ features/volume_features.py
@@ -10,13 +10,18 @@ def add_volume_features(df: pd.DataFrame) -> pd.DataFrame:
     Volume = df['Volume']
 
     # Moving Average
-    df['Vol_ma_5']  = Volume.rolling(window=5).mean()
-    df['Vol_ma_10'] = Volume.rolling(window=10).mean()
-    df['Vol_ma_20'] = Volume.rolling(window=20).mean()
+    Vol_ma_5  = Volume.rolling(window=5).mean()
+    Vol_ma_10 = Volume.rolling(window=10).mean()
+    Vol_ma_20 = Volume.rolling(window=20).mean()
+
+    df['Vol_ma_5']  = Vol_ma_5
+    df['Vol_ma_10'] = Vol_ma_10
+    df['Vol_ma_20'] = Vol_ma_20
 
     # Volume ratio
-    df['Vol_ratio_5']  = Volume / df['Vol_ma_5']
-    df['Vol_ratio_10'] = Volume / df['Vol_ma_10']
+    df['Vol_ratio_5']  = Volume / Vol_ma_5
+    df['Vol_ratio_10'] = Volume / Vol_ma_10
+    df['Vol_ratio_20'] = Volume / Vol_ma_20
 
     # Volume volatility (standard deviation of volume)
     df['Vol_std_5']  = Volume.rolling(window=5).std()
